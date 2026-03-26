be7b7a70f6493cf66102d704120e738a3d8f2674
KimCode <kimlywang08@gmail.com>
2026-03-26 13:28:54 -0400

    Update SIR Simulation.py

--- SIR Simulation.py
+++ SIR Simulation.py
@@ -1,37 +1,37 @@
-# Pseudo code
+# Setup:
+# * Create window (Left = Simulation, Right = Graph).
+# * Create a population as a list of Human objects.
+# * Create 3 lists to store daily S, I, and R counts:
+# susceptible_data, infected_data, recovered_data
+# * Set the first person as "Infected" (patient zero).
 
-# Setup : 
-#   * Create window (Split: Left = Simulation, Right = Graph).
-#   * Population = List of person objects.
-#   * Data = List to store S, I, R counts per day of simulation
-#   * Set first "Infected" person.
-# Dots :
-#   * For each person:
-#     * Update position (x + dx, y + dy).
-#     * Bounce if hitting simulation boundaries.
-#     * If status is "Infected":
-#       * If time_elapsed > Recovery_Threshold:
-#         * Change status to "Recovered".
-#
-#   * For each "Infected" person:
-#     * For each "Susceptible" Person:
-#       * If distance(Infected, Susceptible) < Infection_radius:
-#         * If random_chance < p:
-#           * Change "Susceptible" status to "Infected".
-#           * Set Infection_Timer = current_time. 
-# Data Tracking :
-#   * Every day of simulation:
-#     * Count total S, I, and R agents.
-#     * Add counts to Data List.
-#
-# Visualization
-#   * Draw simulation (Dots moving and changing color).
-#   * Draw Graph:
-#    * Plot S (Blue), I (Red), and R (Green) lines based on Data
-#   * Draw UI:
-#    * Display Title: "Modeling SARS-CoV-2 Propagation Using the SIR Model".
-#    * Display Live Counts and Day Clock.
-#    * Display Legend
+# Main loop:
+# * For each person:
+# * Update position (x + dx, y + dy).
+# * Bounce if hitting simulation boundaries.
+# * If status is "Infected" and infected long enough:
+# * Change status to "Recovered".
+
+# * Make a list of infected people and a list of susceptible people.
+# * For each infected person:
+# * For each susceptible person:
+# * If distance < infection radius:
+# * If random chance < infection rate:
+# * Change susceptible person to "Infected".
+# * Set infection_timer = current frame.
+
+# Data tracking:
+# * Every 60 frames (= 1 simulated day):
+# * Count total susceptible, infected, and recovered people.
+# * Append each count to its corresponding list.
+# * Increase day by 1.
+
+# Visualization:
+# * Draw moving dots in simulation area.
+# * Draw SIR graph using stored daily data.
+# * Show title, live S/I/R counts, current day, and legend.
+# * Stop updating once the simulation reaches the day cap.
+
 
 import math
 import pygame
